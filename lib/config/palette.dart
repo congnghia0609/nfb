25893717858025556a62edd0d53f5e70676b118b
congnghia0609 <congnghia0609@gmail.com>
2020-09-26 16:15:11 +0700

    first commit

---
+++ lib/config/palette.dart
@@ -0,0 +1,39 @@
+/*
+ * Copyright 2020 nghiatc.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+///
+/// @author nghiatc
+/// @since Sep 26, 2020
+
+import 'package:flutter/material.dart';
+
+class Palette {
+  static const Color scaffold = Color(0xFFF0F2F5);
+
+  static const Color facebookBlue = Color(0xFF1777F2);
+
+  static const LinearGradient createRoomGradient = LinearGradient(
+    colors: [Color(0xFF496AE1), Color(0xFFCE48B1)],
+  );
+
+  static const Color online = Color(0xFF4BCB1F);
+
+  static const LinearGradient storyGradient = LinearGradient(
+    begin: Alignment.topCenter,
+    end: Alignment.bottomCenter,
+    colors: [Colors.transparent, Colors.black26],
+  );
+}
---
+++ lib/models/story_model.dart
@@ -0,0 +1,34 @@
+/*
+ * Copyright 2020 nghiatc.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+///
+/// @author nghiatc
+/// @since Sep 26, 2020
+
+import 'package:meta/meta.dart';
+import 'package:nfb/models/models.dart';
+
+class Story {
+  final User user;
+  final String imageUrl;
+  final bool isViewed;
+
+  const Story({
+    @required this.user,
+    @required this.imageUrl,
+    this.isViewed = false,
+  });
+}
