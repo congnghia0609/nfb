5ee32896f3ae86a744d775fbcfa6758cdbd099f4
congnghia0609 <congnghia0609@gmail.com>
2020-09-26 16:16:39 +0700

    first commit

--- lib/data/data.dart
+++ lib/data/data.dart
@@ -18,7 +18,7 @@
 /// @author nghiatc
 /// @since Sep 26, 2020
 
-import 'package:flutter/material.dart';
+import 'package:nfb/models/models.dart';
 
 final User currentUser = User(
   name: 'Marcus Ng',
